9b8a5a8d6fd21314718bf9928d1724c74ed16bf2
Mr.Slime <Slime.kostia@gmail.com>
2025-10-24 23:14:12 +0300

    created 2 requests

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+import aiofiles
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import JSONResponse, FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 from db.database import connect_databases, disconnect_databases
 from routers import artifacts
 import uvicorn
+from db.load_items import get_findings_from_db
+from core.analysis_logic import generate_text_from_multimodal
 
 app = FastAPI(title="Vyshyvka API")
 
@@ -18,6 +23,20 @@ app.include_router(artifacts.router)
 @app.get("/")
 async def root():
     return {"message": "API is running"}
+
+@app.get("/api/get-items")
+async def get_items():
+    return {"text": get_findings_from_db()}
+
+@app.post("/upload/text")
+async def upload_image_and_get_text(file: UploadFile = File(...)):
+    try:
+    
+        image_bytes = await file.read()
+
+        return {"text": generate_text_from_multimodal(image_bytes)}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Не вдалося завантажити файл: {e}")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
